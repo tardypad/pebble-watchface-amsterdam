40eb23f9c2300d79266079fb6a947a21210d5cf5
damien <damien@tardypad.me>
2015-12-28 23:59:13 +0100

    add LICENSE

--- src/amsterdam.c
+++ src/amsterdam.c
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2015 Damien Tardy-Panis
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ **/
+
 #include <pebble.h>
 
 //#define DEBUG_SLOW_ANIMATIONS
