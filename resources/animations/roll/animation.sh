0df4afda23d43072cc276f186b0d39db2084dc36
damien <damien@tardypad.me>
2016-02-20 19:36:30 +0100

    use latest version of gif2apng (version 1.9)
    use z0 option as specified in Pebble docs
    this version seems to not support absolute file path...

--- resources/animations/roll/animation.sh
+++ resources/animations/roll/animation.sh
@@ -2,8 +2,10 @@
 
 PLATFORM="$1"
 ANIMATION_DIR="$(dirname "$(readlink -f "$0")")"
-FRAMES_DIR="$ANIMATION_DIR/$PLATFORM"
-RESULT_FILE="$ANIMATION_DIR/xxx~$PLATFORM"
+FRAMES_DIR="$PLATFORM"
+RESULT_FILE="xxx~$PLATFORM"
+
+cd "$ANIMATION_DIR"
 
 convert -loop 1 -dispose previous -background none \
   -delay 10 "$FRAMES_DIR/01.svg" \
@@ -19,7 +21,8 @@ convert -loop 1 -dispose previous -background none \
   -delay 10 "$FRAMES_DIR/01.svg" \
   "$RESULT_FILE.gif"
 
-gif2apng "$RESULT_FILE.gif" > /dev/null
+gif2apng -z0 "$RESULT_FILE.gif" "$RESULT_FILE.apng" > /dev/null
 
-mv "$RESULT_FILE.png" "$RESULT_FILE.apng"
 rm "$RESULT_FILE.gif"
+
+cd - > /dev/null
